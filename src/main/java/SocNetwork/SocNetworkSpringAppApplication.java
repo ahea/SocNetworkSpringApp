79674250c2b8f56fa1683bbe3aca905dced1a602
Aleksei <Heallex96@gmail.com>
2017-02-11 16:31:17 +0300

    Initial commit

---
+++ src/main/java/SocNetwork/SocNetworkSpringAppApplication.java
@@ -0,0 +1,12 @@
+package SocNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocNetworkSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocNetworkSpringAppApplication.class, args);
+	}
+}
