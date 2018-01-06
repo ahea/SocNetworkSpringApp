7b9a80808e9d78a64ab35dd30aa2a891f8cf5903
Aleksei <heallex96@gmail.com>
2018-01-06 19:22:24 +0300

    [task-3-Search] : implement new version of search by parameters

--- src/main/java/SocNetwork/models/other/SearchRequestParams.java
+++ src/main/java/SocNetwork/models/other/SearchRequestParams.java
@@ -11,12 +11,40 @@ import java.util.Map;
 
 public class SearchRequestParams {
 
+    private Long id;
+    private String email;
+    private String name;
+
     private Map<LanguageName, LanguageLevel> languages = new HashMap<>();
     private Country country;
     private Integer age;
     private Gender gender;
     private boolean hasPhoto;
     private boolean online;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Map<LanguageName, LanguageLevel> getLanguages() {
         return languages;
--- src/main/java/SocNetwork/repositories/UserRepository.java
+++ src/main/java/SocNetwork/repositories/UserRepository.java
@@ -6,16 +6,24 @@ import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
+import java.util.List;
 
 
 public interface UserRepository extends GraphRepository<User> {
 
     User findByEmail(String email);
 
-
     @Query("MATCH (a:User)-[r:HAS_IN_FRIENDLIST]->(b:User) " +
             "WHERE id(b) = {userId} " +
             "RETURN a")
     Collection<User> getIncomingFriendsById (@Param("userId") Long id);
 
+    List<User> findByName(String name);
+
+    @Query("MATCH (a:User)-[r:HAS_LANGUAGE]->(b:Language) " +
+            "WHERE b.languageName = {languageName} " +
+            "AND   r.level = {languageLevel} " +
+            "RETURN a")
+    Collection<User> findSpeakers(@Param("languageName")  String languageName,
+                                  @Param("languageLevel") String languageLevel);
 }
